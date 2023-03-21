369eb4ccf2dc4b405cd2f9a9ee30c52960570ed8
sayando <andousayuka@gmail.com>
2023-03-21 22:47:15 +0900

    add Review

---
+++ src/Review01.java
@@ -0,0 +1,14 @@
+public class Review01 {
+    public static void main(String [] args) {
+        int price = 1500;
+        int taxPrice;
+        taxPrice = tax(price);
+        int totalPrice = price + taxPrice;
+System.out.println(price+"円の商品の税込価格は"+totalPrice+"円（消費税は"+taxPrice+"円）です。");
+}
+    public static int tax(int price) {
+        int taxRate = 10;
+        int taxPrice = price / taxRate;
+        return taxPrice;
+}
+}
